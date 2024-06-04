e7705b35ded5cfcddd4e9bdff7af04b95977b3f4
Adrian Hallen <hallen412120@gmail.com>
2024-06-04 10:22:49 -0400

    MediaAdapter updates

--- app/src/main/java/com/hallen/rfilemanager/ui/view/adapters/media/MediaAdapter.kt
+++ app/src/main/java/com/hallen/rfilemanager/ui/view/adapters/media/MediaAdapter.kt
@@ -3,11 +3,14 @@ package com.hallen.rfilemanager.ui.view.adapters.media
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.hallen.rfilemanager.databinding.ItemMediaBinding
 import com.hallen.rfilemanager.infraestructure.utils.ImageController
 import com.hallen.rfilemanager.model.Archivo
+import com.hallen.rfilemanager.model.UpdateModel
 import com.hallen.rfilemanager.ui.view.adapters.main.AdapterListener
+import com.hallen.rfilemanager.ui.view.adapters.main.MainDiff
 import javax.inject.Inject
 
 class MediaAdapter @Inject constructor(private var imageController: ImageController) :
@@ -16,6 +19,9 @@ class MediaAdapter @Inject constructor(private var imageController: ImageControl
 
     private var files: List<Archivo> = emptyList()
     private var listeners: AdapterListener? = null
+    fun setListeners(listeners: AdapterListener?){
+        this.listeners = listeners
+    }
     override fun getItemCount(): Int = files.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
@@ -26,6 +32,18 @@ class MediaAdapter @Inject constructor(private var imageController: ImageControl
 
     override fun onBindViewHolder(holder: MediaViewHolder, position: Int) =
         holder.bind(files[position])
+
+    fun update(updateModel: UpdateModel) {
+        if (updateModel.reloadAll){
+            files = updateModel.files
+            notifyDataSetChanged()
+            return
+        }
+        val diffUtil = MainDiff(files, updateModel.files)
+        val calculateDiff = DiffUtil.calculateDiff(diffUtil)
+        calculateDiff.dispatchUpdatesTo(this)
+        files = updateModel.files
+    }
 }
 
 class MediaViewHolder(
--- app/src/main/java/com/hallen/rfilemanager/ui/view/fragments/MediaFragment.kt
+++ app/src/main/java/com/hallen/rfilemanager/ui/view/fragments/MediaFragment.kt
@@ -6,14 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.hallen.rfilemanager.databinding.FragmentMediaBinding
+import com.hallen.rfilemanager.ui.view.adapters.main.AdapterListener
+import com.hallen.rfilemanager.ui.view.adapters.media.MediaAdapter
 import com.hallen.rfilemanager.ui.viewmodels.BaseViewModel
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MediaFragment : Fragment() {
+class MediaFragment : Fragment(), AdapterListener {
     private val baseViewModel: BaseViewModel by viewModels()
     private lateinit var binding: FragmentMediaBinding
+
+    @Inject
+    lateinit var adapter: MediaAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -29,13 +37,32 @@ class MediaFragment : Fragment() {
     }
 
     private fun configureRecyclerView() {
-
+        adapter.setListeners(this)
+        binding.mediaRecyclerView.setHasFixedSize(true)
+        binding.mediaRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3, GridLayoutManager.VERTICAL, false)
+        binding.mediaRecyclerView.adapter = adapter
     }
 
     private fun configureObservers() {
         baseViewModel.mode.observe(viewLifecycleOwner) {
 
         }
+        baseViewModel.update.observe(viewLifecycleOwner){
+            Logger.i("Observing changes from fragmet")
+            adapter.update(it)
+        }
+    }
+
+    override fun onClick(adapterPosition: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLongClick(adapterPosition: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCheck(adapterPosition: Int) {
+        TODO("Not yet implemented")
     }
 
 }
